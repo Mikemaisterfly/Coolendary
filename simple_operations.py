16e4b4672d9f1606820d0e771c42855003fc67fc
Ibagra <79232672+Ibagra@users.noreply.github.com>
2021-11-21 16:41:52 +0100

    Update simple_operations.py

--- simple_operations.py
+++ simple_operations.py
@@ -2,12 +2,12 @@ from datetime import date
 import calendar
 
 
-def fecha():
+def current_date():
     dt = date.today()
     print("{}/{}/{}".format(dt.day, dt.month, dt.year) + " and the current day is " + dt.strftime("%A"))
 
 
 def month_calendar():
     dt = date.today()
-    calendario = calendar.TextCalendar(calendar.MONDAY)
-    print(calendario.formatmonth(dt.year, dt.month))
+    c = calendar.TextCalendar(calendar.MONDAY)
+    print(c.formatyear(dt.year, 2, 1, 6, 4))
