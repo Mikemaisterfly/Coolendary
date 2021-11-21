d3728f4f0b7a6965174c97d8a6c4bf591410a2f0
Ibagra <79232672+Ibagra@users.noreply.github.com>
2021-11-21 16:07:33 +0100

    Update simple_operations.py

--- simple_operations.py
+++ simple_operations.py
@@ -2,12 +2,12 @@ from datetime import date
 import calendar
 
 
-def current_day():
+def fecha():
     dt = date.today()
     print("{}/{}/{}".format(dt.day, dt.month, dt.year) + " and the current day is " + dt.strftime("%A"))
 
 
-def coolendar():
+def month_calendar():
     dt = date.today()
     calendario = calendar.TextCalendar(calendar.MONDAY)
     print(calendario.formatmonth(dt.year, dt.month))
