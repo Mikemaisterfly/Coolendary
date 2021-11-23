d05031fc2ad027e456a9fd6eea9d9350e0be00e4
Ibagra <79232672+Ibagra@users.noreply.github.com>
2021-11-23 18:03:18 +0100

    Update simple_operations.py

--- simple_operations.py
+++ simple_operations.py
@@ -2,12 +2,22 @@ from datetime import date
 import calendar
 
 
-def current_date():
+def print_current_date():
+    """
+    Print the current date with this format: {day}/{month}/{year} and the weekday.
+    :return: none
+    """
     dt = date.today()
     print("{}/{}/{}".format(dt.day, dt.month, dt.year) + " and the current day is " + dt.strftime("%A"))
 
 
-def month_calendar():
-    dt = date.today()
+def print_calendar(year: int = date.today().year, month: int = date.today().month):
+    """
+    Print a calendar of the especified month and year or the current ones if you do not write any number.
+    :param year: current year or especified year
+    :param month: current month or especified month
+    :return: none
+    """
     c = calendar.TextCalendar(calendar.MONDAY)
-    print(c.formatyear(dt.year, 2, 1, 6, 4))
+    print(c.formatmonth(year, month))
+
